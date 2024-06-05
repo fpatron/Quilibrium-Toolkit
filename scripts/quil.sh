e30eaa289240584d68178903865058fe478da4c7
CryptoSeb <115431687+Sebcrypto@users.noreply.github.com>
2024-06-05 14:56:18 +0200

    Update quil.sh

--- scripts/quil.sh
+++ scripts/quil.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
 
-# Fonction pour afficher l'utilisation du script
+# Display the commands the script can run
 usage() {
-    echo "Usage: $0 {start|stop|restart|journal}"
+    echo "Usage: $0 {start|stop|restart|journal|frame|dif}"
+    echo "quil start   : The script will start the quilibrium node"
+    echo "quil stop    : The script will stop the quilibrium node"
+    echo "quil restart : The script will restart the quilibrium node"
+    echo "quil journal : The script will display quilibrium logs"
+    echo "quil frame   : The script will display frame number"
+    echo "quil dif     : The script will display current difficulty"
     exit 1
 }
 
-# Si aucun argument n'est fourni, lancez le journal par défaut
+# if no argument is passed, script will display quilibrium logs
 if [ -z "$1" ]; then
     journalctl -u quilibrium -f
     exit 0
 fi
 
-# Exécuter la commande appropriée en fonction de l'argument fourni
+# "frame" : The script will display frame number
 case "$1" in
     frame)
-        # avec le nom de la machine
-        # journalctl -u quilibrium -n 100 | grep "current_frame" | while read line; do echo "$line" | grep -o '^[^ ]\+ \+[^ ]\+ \+[^ ]\+ \+[^ ]\+' | tr -d '\n'; echo " $(echo "$line" | grep -o '"current_frame":[0-9]\+')" ; done
-        journalctl -u quilibrium -n 100 | grep "current_frame" | while read line; do echo "$line" | grep -o '^[^ ]\+ \+[^ ]\+ \+[^ ]\+' | tr -d '\n'; echo " $(echo "$line" | grep -o '"current_frame":[0-9]\+')" ; done
-        
-        journalctl -u quilibrium -f | stdbuf -oL grep "current_frame" | while read -r line; do 
-#            echo "$line" | grep -o '^[^ ]\+ \+[^ ]\+ \+[^ ]\+ \+[^ ]\+' | tr -d '\n'; 
+        journalctl -u quilibrium -n 100 -f | stdbuf -oL grep "current_frame" | while read -r line; do 
             echo "$line" | grep -o '^[^ ]\+ \+[^ ]\+ \+[^ ]\+' | tr -d '\n'; 
-                echo " $(echo "$line" | grep -o '"current_frame":[0-9]\+')"; 
+            echo " $(echo "$line" | grep -o '"current_frame":[0-9]\+')"; 
         done
         ;;
+# "dif" : The script will display current difficulty
     dif)
-        journalctl -u quilibrium -n 100 | grep "next_difficulty_metric" | while read line; do echo "$line" | grep -o '^[^ ]\+ \+[^ ]\+ \+[^ ]\+' | tr -d '\n'; echo " $(echo "$line" | grep -o '"next_difficulty_metric":[0-9]\+')" ; done
-        journalctl -u quilibrium -f | stdbuf -oL grep "next_difficulty_metric" | while read -r line; do 
+        journalctl -u quilibrium -n 100 -f | stdbuf -oL grep "next_difficulty_metric" | while read -r line; do 
             echo "$line" | grep -o '^[^ ]\+ \+[^ ]\+ \+[^ ]\+' | tr -d '\n'; 
             echo " $(echo "$line" | grep -o '"next_difficulty_metric":[0-9]\+')"; 
         done
         ;;
+# "start" : The script will start the quilibrium node
     start)
-        systemctl start quilibrium
+        sudo systemctl start quilibrium
         ;;
+# "stop" : The script will stop the quilibrium node
     stop)
-        systemctl stop quilibrium
+        sudo systemctl stop quilibrium
         ;;
+# "restart" : The script will restart the quilibrium node
     restart)
-        systemctl restart quilibrium
+        sudo systemctl restart quilibrium
         ;;
+# "journal" : The script will display quilibrium logs
     journal)
         journalctl -u quilibrium -f
         ;;
